03adaab9a1c964e807a4afc62a0fc9f19af97b8d
Saurabh-del <gautamsaurabh199@gmail.com>
2020-12-16 23:21:05 +0530

    Update Version

--- calender/settings.py
+++ calender/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@x_&2rztuso^bx0o6i_-&uw97pkmzxowjj=*a4caku=ofe8ln8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['caldjango.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
